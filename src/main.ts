f6240ee6f1db6d4d8f068cf854fb77838693fd27
nebiros88 <nebiros1988@gmail.com>
2026-06-09 20:09:46 +0200

    task-8: refactoring main.ts - return lambda handler

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
 
+import { Callback, Context, Handler } from 'aws-lambda';
 import helmet from 'helmet';
-
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
-
-  const port = configService.get('APP_PORT') || 4000;
 
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
+
   app.use(helmet());
 
-  await app.listen(port, () => {
-    console.log('App is running on %s port', port);
-  });
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: unknown,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
