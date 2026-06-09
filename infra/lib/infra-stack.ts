a6998fd84bb42c7c24203f92ccf1fd378225d1bb
nebiros88 <nebiros1988@gmail.com>
2026-06-09 20:34:24 +0200

    task-8: configure nest cdk stack

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigwv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const nestLambda = new NodejsFunction(this, 'NestLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'main.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist')),
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 256,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigwv2.HttpApi(this, 'NestHttpApi', {
+      apiName: 'nodejs-aws-cart-api',
+      corsPreflight: {
+        allowOrigins: ['*'],
+        allowMethods: [apigwv2.CorsHttpMethod.ANY],
+        allowHeaders: ['*'],
+      },
+    });
+
+    const integration = new integrations.HttpLambdaIntegration(
+      'NestLambdaIntegration',
+      nestLambda,
+    );
+
+    api.addRoutes({
+      path: '/',
+      methods: [apigwv2.HttpMethod.ANY],
+      integration,
+    });
+
+    api.addRoutes({
+      path: '/{proxy+}',
+      methods: [apigwv2.HttpMethod.ANY],
+      integration,
+    });
+
+    new cdk.CfnOutput(this, 'NestApiUrl', {
+      value: api.url ?? '',
+    });
   }
 }
